eb8d1912abf5e74b2ff39ac5ae74fc1e40d81d04
Peter Koraca <peter.koraca@gmail.com>
2025-11-30 08:52:41 +0000

    added fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,11 @@ export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 	modules: ["@nuxtjs/supabase"],
+
+	// Nitro options
+	nitro: {
+		externals: {
+			inline: ["@supabase/supabase-js"], // this fixes the issue
+		},
+	},
 });
